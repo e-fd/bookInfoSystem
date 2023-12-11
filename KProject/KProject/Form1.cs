4b24592f330db66242d68520b27d37e611b899c1
Elena <elenafedoseeva.r@gmail.com>
2023-12-11 18:23:17 +0400

    Fixed array issue;

--- KProject/KProject/Form1.cs
+++ KProject/KProject/Form1.cs
@@ -1,16 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace KProject
 {
     public partial class Form1 : Form
     {
-        public Book[] books;
+        Book[] books = new Book[1000];
         public Form1()
         {
             InitializeComponent();
         }
-        private void ‰Ó·‡‚ËÚ¸ ÌË„ÛToolStripMenuItem_Click(object sender, EventArgs e)
+
+        private void ‰Ó·‡‚ËÚ¸ ÌË„ÛToolStripMenuItem_Click_1(object sender, EventArgs e)
         {
             AddBook addBook = new AddBook();
-            addBook.Show();
+            addBook.ShowDialog();
+            if (addBook.ReturnFlag())
+            {
+                Book book = new Book(addBook.ReturnTitle(), addBook.ReturnAutor());
+                books[book.BookId] = book;
+                listBox1.Items.Add(book.Author+"  "+book.Title);
+            }
+            addBook.Close();
         }
     }
+
 }
