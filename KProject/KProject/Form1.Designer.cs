4d33221d39ab9eedb574049724a07b824cc9ff5d
Elena <elenafedoseeva.r@gmail.com>
2023-12-13 04:32:45 +0400

    Addded comments;

--- KProject/KProject/Form1.Designer.cs
+++ KProject/KProject/Form1.Designer.cs
@@ -47,6 +47,17 @@ namespace KProject
             openFileDialog1 = new OpenFileDialog();
             saveFileDialog1 = new SaveFileDialog();
             listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            button3 = new Button();
+            label3 = new Label();
+            comboBox1 = new ComboBox();
+            button4 = new Button();
+            button5 = new Button();
             menuStrip1.SuspendLayout();
             statusStrip1.SuspendLayout();
             SuspendLayout();
@@ -71,27 +82,27 @@ namespace KProject
             // создатьToolStripMenuItem
             // 
             создатьToolStripMenuItem.Name = "создатьToolStripMenuItem";
-            создатьToolStripMenuItem.Size = new Size(224, 26);
+            создатьToolStripMenuItem.Size = new Size(166, 26);
             создатьToolStripMenuItem.Text = "Создать";
             // 
             // открытьToolStripMenuItem
             // 
             открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
-            открытьToolStripMenuItem.Size = new Size(224, 26);
+            открытьToolStripMenuItem.Size = new Size(166, 26);
             открытьToolStripMenuItem.Text = "Открыть";
             открытьToolStripMenuItem.Click += открытьToolStripMenuItem_Click;
             // 
             // сохранитьToolStripMenuItem
             // 
             сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
-            сохранитьToolStripMenuItem.Size = new Size(224, 26);
+            сохранитьToolStripMenuItem.Size = new Size(166, 26);
             сохранитьToolStripMenuItem.Text = "Сохранить";
             сохранитьToolStripMenuItem.Click += сохранитьToolStripMenuItem_Click;
             // 
             // выходToolStripMenuItem
             // 
             выходToolStripMenuItem.Name = "выходToolStripMenuItem";
-            выходToolStripMenuItem.Size = new Size(224, 26);
+            выходToolStripMenuItem.Size = new Size(166, 26);
             выходToolStripMenuItem.Text = "Выход";
             выходToolStripMenuItem.Click += выходToolStripMenuItem_Click;
             // 
@@ -171,11 +182,125 @@ namespace KProject
             listBox1.Size = new Size(521, 436);
             listBox1.TabIndex = 3;
             // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(23, 72);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(322, 24);
+            textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(23, 99);
+            label1.Name = "label1";
+            label1.Size = new Size(190, 18);
+            label1.TabIndex = 5;
+            label1.Text = "Введите фамилию автора";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(351, 72);
+            button1.Name = "button1";
+            button1.Size = new Size(110, 29);
+            button1.TabIndex = 6;
+            button1.Text = "Поиск";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(351, 118);
+            button2.Name = "button2";
+            button2.Size = new Size(110, 29);
+            button2.TabIndex = 9;
+            button2.Text = "Поиск";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label2.Location = new Point(23, 51);
+            label2.Name = "label2";
+            label2.Size = new Size(186, 18);
+            label2.TabIndex = 8;
+            label2.Text = "Введите заголовок книги";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(23, 120);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(322, 24);
+            textBox2.TabIndex = 7;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.Location = new Point(351, 166);
+            button3.Name = "button3";
+            button3.Size = new Size(110, 29);
+            button3.TabIndex = 12;
+            button3.Text = "Поиск";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label3.Location = new Point(23, 147);
+            label3.Name = "label3";
+            label3.Size = new Size(116, 18);
+            label3.TabIndex = 11;
+            label3.Text = "Выберите жанр";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(22, 166);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(323, 26);
+            comboBox1.TabIndex = 13;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button4.Location = new Point(224, 214);
+            button4.Name = "button4";
+            button4.Size = new Size(237, 29);
+            button4.TabIndex = 14;
+            button4.Text = "Очистить параметры поиска";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button5.Location = new Point(224, 258);
+            button5.Name = "button5";
+            button5.Size = new Size(237, 29);
+            button5.TabIndex = 15;
+            button5.Text = "Поиск по всем параметрам";
+            button5.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 18F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1000, 506);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(comboBox1);
+            Controls.Add(button3);
+            Controls.Add(label3);
+            Controls.Add(button2);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
             Controls.Add(listBox1);
             Controls.Add(statusStrip1);
             Controls.Add(menuStrip1);
@@ -184,7 +309,6 @@ namespace KProject
             Margin = new Padding(4, 3, 4, 3);
             Name = "Form1";
             Text = "Система поиска книг в библиотеке";
-            Activated += Form1_Activated;
             FormClosing += Form1_FormClosing;
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
@@ -219,5 +343,16 @@ namespace KProject
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private ListBox listBox1;
+        private TextBox textBox1;
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Label label2;
+        private TextBox textBox2;
+        private Button button3;
+        private Label label3;
+        private ComboBox comboBox1;
+        private Button button4;
+        private Button button5;
     }
 }
--- KProject/KProject/Form2.cs
+++ KProject/KProject/Form2.cs
@@ -10,55 +10,69 @@ using System.Windows.Forms;
 
 namespace KProject
 {
-    public partial class AddBook : Form
+    public partial class AddBook : Form // окно Добавление новой книги
     {
-        bool Flag = false;
-        public AddBook()
+        bool Flag = false; // false - добавления книги в БД  не происходит,
+                           // true - книга добавляется в БД
+        public AddBook() // конструктор окна Добавление новой книги
         {
-            InitializeComponent();
-            comboBox1.Items.Clear();
-            comboBox1.Items.Add("Неизвестно");
-            comboBox1.Items.Add("Проза");
-            comboBox1.Items.Add("Поэзия");
-            comboBox1.Items.Add("Приключения");
-            comboBox1.Items.Add("Драма");
-            comboBox1.Items.Add("Фэнтези");
-            comboBox1.Items.Add("Научная фантастика");
-            comboBox1.Items.Add("Биография и мемуары");
-            comboBox1.Items.Add("Мифы и легенды");
-            comboBox1.SelectedIndex = 0;            
+            InitializeComponent(); // инициализация компонента - открытие окна, загрузка элементов
+            comboBox1.Items.Clear(); // очистка списка жанров книг
+            comboBox1.Items.Add("Неизвестно");          // добавление жанров книг
+            comboBox1.Items.Add("Проза");               //
+            comboBox1.Items.Add("Поэзия");              //
+            comboBox1.Items.Add("Приключения");         //
+            comboBox1.Items.Add("Драма");               //
+            comboBox1.Items.Add("Фэнтези");             //
+            comboBox1.Items.Add("Научная фантастика");  //
+            comboBox1.Items.Add("Биография и мемуары"); //
+            comboBox1.Items.Add("Мифы и легенды");      //
+            comboBox1.SelectedIndex = 0;                // индекс изначально выбранного элемента
         }
-        public void SetAuthor(string Author)
+        public void SetTitle(string Title) // (1) метод для присвоения значения
+        {                                  // записываемого текста в поле 
+            textBox1.Text = Title;
+        }
+        public string? ReturnTitle() // (2) метод для возврата записанного
+        {                            // в поле текста
+            return textBox1.Text;
+        }
+        public void SetAuthor(string Author) // (1)
         {
             textBox2.Text = Author;
         }
-        public string? ReturnAutor()
+        public string? ReturnAutor() // (2)
         {
             return textBox2.Text;
         }
-        public void SetTitle(string Title)
+        public void SetGenre(string Genre) // (1)
         {
-            textBox1.Text = Title;
+            comboBox1.Text = Genre;
         }
-        public string? ReturnTitle()
+        public string? ReturnGenre() // (2)
         {
-            return textBox1.Text;
+            return comboBox1.Text;
         }
-
-        public bool ReturnFlag()
+        public void SetDescription(string Description) // (1)
+        {
+            richTextBox1.Text = Description;
+        }
+        public string? ReturnDescription() // (2)
+        {
+            return richTextBox1.Text;
+        }
+        public bool ReturnFlag() // метод для возвращения значения переменной Flag
         {
             return Flag;
         }
         private void button2_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-        }
-
-
+        {                // закрытие окна при нажатии кнопки "Отмена"
+            this.Hide(); // (введенные данные не сохраняются
+        }                // и книга не добавляется в БД (Flag == false))
         private void button1_Click(object sender, EventArgs e)
-        {
-            Flag = true;
-            this.Hide();
+        {                // при нажатии кнопки "Добавить" происходит изменение значения
+            Flag = true; // переменной Flag (и, соответственно, добавление книги в список)
+            this.Hide(); // и закрытие окна
         }
     }
 }
