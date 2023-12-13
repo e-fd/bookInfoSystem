859e0e43278fbbf46065f10ff93d99e01c1cabf2
Elena <elenafedoseeva.r@gmail.com>
2023-12-13 04:32:45 +0400

    Addded comments

--- KProject/KProject/Book.cs
+++ KProject/KProject/Book.cs
@@ -8,33 +8,35 @@ namespace KProject
 {
     internal class Book
     {
-        private static int bookCounter;
-        public int BookId { get; set; }
-        public string? Title { get; set; }
-        public string? Author { get; set; }
-        public string? Description { get; set; }
-        public string? Genre { get; set; }
-        public Book(string? title)
+        private static int bookCounter; // статический счетчик книг
+        public int BookId { get; set; }          // свойства объектов класса Book
+        public string? Title { get; set; }       //
+        public string? Author { get; set; }      //
+        public string? Genre { get; set; }       //
+        public string? Description { get; set; } //
+        public Book(string? title)               // (1) конструкторы для объектов класса Book
         {
             Title = title;
-            BookId = bookCounter++;
+            BookId = bookCounter++; // присваивание ID книге при создании объекта
         }
-        public Book(string? title, string? author) : this (title)
-        {
-            Author = author;
+        public Book(string? title, string? author) : this (title) // (1)
+        {                                        // вызов предыдущего
+            Author = author;                     // конструктора
         }
-        public Book(string? title, string? author, string? description) : this(title, author)
+        public Book(string? title, string? author, string? description) : 
+            this(title, author) // (1)
         {
             Description = description;
         }
-        public Book(string? title, string? author, string? description, string? genre) : this(title, author, description)
+        public Book(string? title, string? author, string? description, string? genre) : 
+            this(title, author, description) // (1)
         {
             Genre = genre;
         }
         public void BookInfo(out int bookId, out string? title, out string? author, 
-                                out string? description, out string? genre)
+                                out string? description, out string? genre) // ?  .
         {
-            bookId = BookId;
+            bookId = BookId; 
             title = Title;
             author = Author;
             description = Description;
--- KProject/KProject/Form1.cs
+++ KProject/KProject/Form1.cs
@@ -13,84 +13,84 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace KProject
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form // окно Системы поиска книг в библиотеке
     {
-        Book[] books = new Book[1000];
-        public Form1()
+        Book[] books = new Book[1000]; // массив книг
+        public Form1() // конструктор окна Системы поиска книг в библиотеке
         {
-            InitializeComponent();
-            string fileName = "Default.db";
+            InitializeComponent(); // инициализация компонента - открытие окна, загрузка элементов
+            string fileName = "Default.db"; // дефолтная база данных
             try
             {
                 using (var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
-                {
+                {                           // открытие файла дефолтной базы данных
                     using (var sr = new StreamReader(fs/*, Encoding.GetEncoding(1251)*/))
-                    {
-                        listBox1.Items.Clear();
-
-                        while (!sr.EndOfStream)
+                    {                   // чтение файла дефолтной базы данных
+                        listBox1.Items.Clear(); // очистка списка книг
+                        while (!sr.EndOfStream) // чтение файла до конца
                         {
-                            string[] buffer = sr.ReadLine().Split('|');
-
-                            listBox1.Items.Add(buffer[0] + "  " + buffer[1]);
-                            Book book = new Book(buffer[0], buffer[1]);
+                            string[] buffer = sr.ReadLine().Split('|'); // чтение строки файла 
+                                    // и сохранение каждого набора символов, разделенных '|',
+                                                                        // в массив
+                            listBox1.Items.Add(" \"" + buffer[0] + "\"  " + buffer[1]);
+                                                                // пополнение списка книг
+                            Book book = new Book(buffer[0], buffer[1], buffer[2], buffer[3]);
+                            // создание нового объекта класса Book, заполнение всех его параметров
                             books[book.BookId] = book;
                         }
                     }
                 }
             }
-            catch { }
+            catch { } // файл БД не загрузится в случае ошибки; список книг окажется пустым
         }
 
         private void добавитьКнигуToolStripMenuItem_Click_1(object sender, EventArgs e)
-        {
-            AddBook addBook = new AddBook();
-            addBook.ShowDialog();
-            if (addBook.ReturnFlag())
-            {
-                Book book = new Book(addBook.ReturnTitle(), addBook.ReturnAutor());
-                books[book.BookId] = book;
-                listBox1.Items.Add(book.Author + "  " + book.Title);
+        {           // метод, срабатывающий при нажатии 'редактировать / добавить книгу'
+            AddBook addBook = new AddBook();            // создание нового объекта окна Добавление новой книги
+            addBook.ShowDialog();                       // открытие окна Добавление новой книги
+            if (addBook.ReturnFlag())                   // если кнопка "Добавить" была нажата
+            { // создание нового объекта класса Book, заполнение всех его параметров
+                Book book = new Book(addBook.ReturnTitle(), addBook.ReturnAutor(),
+                    addBook.ReturnGenre(), addBook.ReturnDescription());
+                books[book.BookId] = book;                    // запись созданного объекта в массив книг
+                listBox1.Items.Add(" \"" + book.Title + "\"  " + book.Author); // пополнение списка книг
             }
-            addBook.Close();
+            addBook.Close();        // закрытие окна Добавление новой книги
         }
 
         private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            var sourceFileOpenFileDialog = new OpenFileDialog
+        { // метод, срабатывающий при нажатии 'файл / открыть'
+            var sourceFileOpenFileDialog = new OpenFileDialog // инициализация объекта класса OpenFileDialog
             {
                 //InitialDirectory = @"C:\",
-                Filter = @"Файл базы данных(*.db)|*.db",
-                RestoreDirectory = true,
-                Multiselect = false,
-                Title = @"Открыть файл базы данных"
+                Filter = @"Файл базы данных(*.db)|*.db", // фильтр расширения открываемого файла
+                RestoreDirectory = true, // диалоговое окно запомнит директорию выбранного файла
+                Multiselect = false, // пользователь не может выбрать более одного файла
+                Title = @"Открыть файл базы данных" // название диалогового окна
             };
 
-            if (sourceFileOpenFileDialog.ShowDialog() == DialogResult.OK)
-            {
+            if (sourceFileOpenFileDialog.ShowDialog() == DialogResult.OK) // если пользователь выбрал файл
+            {                                                           // и нажал кнопку "Открыть",
                 try
-                {
+                { // производится открытие, чтение файла
                     string fileName = sourceFileOpenFileDialog.FileName;
 
                     using (var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
                     {
                         using (var sr = new StreamReader(fs/*, Encoding.GetEncoding(1251)*/))
-                        {
+                        { // очистка открытого ранее списка книг
                             listBox1.Items.Clear();
-
-                            while (!sr.EndOfStream)
-                            {
+                            while (!sr.EndOfStream) // список книг в окне Система поиска книг в библиотеке
+                            { // заполняется книгами из нового файла, и новые объекты добавляются в массив books
                                 string[] buffer = sr.ReadLine().Split('|');
-
-                                listBox1.Items.Add(buffer[0] + "  " + buffer[1]);
-                                Book book = new Book(buffer[0], buffer[1]);
+                                listBox1.Items.Add(" \"" + buffer[0] + "\"  " + buffer[1]);
+                                Book book = new Book(buffer[0], buffer[1], buffer[2], buffer[3]);
                                 books[book.BookId] = book;
                             }
-
                         }
                     }
                 }
-                catch (Exception ex)
+                catch (Exception ex) // вывод сообщения при возникновении ошибки открытия файла
                 {
                     MessageBox.Show(@"Ошибка: невозможно открыть файл. " + ex.Message);
                 }
@@ -98,27 +98,28 @@ namespace KProject
         }
 
         private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            var sourseFileSFD = new SaveFileDialog
+        { // метод, срабатывающий при нажатии 'файл / сохранить'
+            var sourceFileSFD = new SaveFileDialog // инициализация объекта класса SaveFileDialog
             {
                 //InitialDirectory = @"C:\",
-                Filter = @"Файл базы данных (*.db)|*.db",
-                RestoreDirectory = true,
-                Title = @"Записать файл с данными о книгах"
+                Filter = @"Файл базы данных (*.db)|*.db", // фильтр расширения сохраняемого файла
+                RestoreDirectory = true, // диалоговое окно запомнит директорию выбранного файла
+                Title = @"Записать файл с данными о книгах" // название диалогового окна
             };
-            if (sourseFileSFD.ShowDialog() == DialogResult.OK)
-            {
+            if (sourceFileSFD.ShowDialog() == DialogResult.OK) // если пользователь выбрал файл
+            {                                                  // и нажал кнопку "Сохранить",
                 try
-                {
-                    using (var fs = new FileStream(sourseFileSFD.FileName, FileMode.Create, FileAccess.Write))
+                { // производится создание файла,
+                    using (var fs = new FileStream(sourceFileSFD.FileName, FileMode.Create, FileAccess.Write))
                     {
                         using (var sw = new StreamWriter(fs/*, Encoding.GetEncoding("Windows - 1251")*/))
                         {
                             foreach (var book in books)
-                            {
+                            { // запись всех полей элементов массива books в сохраняемый файл
                                 if (book != null)
                                 {
-                                    string outputString = book.Author + "|" + book.Title + "|";
+                                    string outputString = book.Author + "|" + book.Title + "|"
+                                        + book.Genre + "|" + book.Description + "|";
                                     sw.WriteLine(outputString);
                                 }
                             }
@@ -126,29 +127,24 @@ namespace KProject
                     }
 
                 }
-                catch (Exception ex)
+                catch (Exception ex) // вывод сообщения при возникновении ошибки сохранения файла
                 {
-                    MessageBox.Show(@"Ошибка: не удается открыть файл. " + ex.Message);
+                    MessageBox.Show(@"Ошибка: не удается сохранить файл. " + ex.Message);
                 }
             }
         }
-
-        private void Form1_Activated(object sender, EventArgs e)
-        {
-
-        }
-
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
+        { // метод, срабатывающий при закрытии окна Система поиска книг в библиотеке
             using (var fs = new FileStream("Default.db", FileMode.Create, FileAccess.Write))
-            {
+            {                                                   // запись в файл дефолтной БД
                 using (var sw = new StreamWriter(fs/*, Encoding.GetEncoding("Windows - 1251")*/))
                 {
                     foreach (var book in books)
-                    {
+                    { // запись всех полей элементов массива books в файл дефолтной БД 
                         if (book != null)
                         {
-                            string outputString = book.Author + "|" + book.Title + "|";
+                            string outputString = book.Title + "|" + book.Author + "|"
+                                 + book.Genre+ "|" + book.Description+ "|";
                             sw.WriteLine(outputString);
                         }
                     }
@@ -157,28 +153,45 @@ namespace KProject
         }
 
         private void выходToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            this.Close();
+        { // метод, срабатывающий при нажатии 'файл / выход'
+            this.Close(); // закрытие окна Система поиска книг в библиотеке
         }
 
         private void редактироватьКнигуToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            AddBook addBook = new AddBook();
-            int index = listBox1.SelectedIndex;
-            if (index != -1)
-            {
-                addBook.SetTitle(books[index].Title);
-                addBook.SetAuthor(books[index].Author);
-                addBook.ShowDialog();
-                if (addBook.ReturnFlag())
-                {
-                    books[index].Author = addBook.ReturnAutor();
-                    books[index].Title = addBook.ReturnTitle();
-                    listBox1.Items.Insert(index, books[index].Author + "  " + books[index].Title);
-                    listBox1.Items.Remove(index);
+        { // метод, срабатывающий при нажатии 'редактировать / редактировать книгу'
+            AddBook addBook = new AddBook(); // создание нового объекта окна Добавление новой книги
+            int index = listBox1.SelectedIndex; // переменная, которой присваивается значение 
+            if (index != -1)                    // индекса выбранной для редактирования книги
+            {                                                     // присваивание текстовым полям окна Добавление                addBook.SetTitle(books[index].Title);
+                addBook.SetTitle(books[index].Title);             // новой книги заданных значений
+                addBook.SetAuthor(books[index].Author);           //
+                addBook.SetGenre(books[index].Genre);             //
+                addBook.SetDescription(books[index].Description); //
+                addBook.ShowDialog();                             // открытие окна Добавление новой книги
+                if (addBook.ReturnFlag())                         // если кнопка "Добавить" была нажата
+                {                                                 
+                    books[index].Title = addBook.ReturnTitle();             // изменение значений полей выбранного 
+                    books[index].Author = addBook.ReturnAutor();            // элемента массива
+                    books[index].Genre = addBook.ReturnGenre();             // 
+                    books[index].Description = addBook.ReturnDescription(); //
+                    //listBox1.Items.Insert(index, " \"" + books[index].Title + "\"  " + books[index].Author);
+                    //listBox1.Items.Remove(index + 1);
+                    listBox1.Items.Clear();         // очистка списка книг
+                    foreach (var book in books) // для каждого элемента (book) массива books
+                    {
+                        if (book != null)       // если элемент book не пустой
+                        {                       // заполяем список книг, используя поля элементов массива books
+                            listBox1.Items.Add(" \"" + book.Title + "\"  " + book.Author);
+                        }
+                    }
                 }
             }
-            addBook.Close();
+            addBook.Close(); // закрытие окна Добавление новой книги
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { // метод, срабатывающий при поиске по заголовку книги
+
         }
     }
 
